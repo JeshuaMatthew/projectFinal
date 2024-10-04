ec83f55aad64f6a2f91c38da4e41985a8ee43b1e
Jeheskiel Kaja <2381052@unai.edu>
2024-10-04 17:44:38 +0700

    Main & AdminMenu

--- aplikasipendataanpenyewaankamera/AdminMenu.java
+++ aplikasipendataanpenyewaankamera/AdminMenu.java
@@ -10,17 +10,19 @@ public class AdminMenu {
     }
 
     private void showMenu(){
+        System.out.println("=============================================");
         System.out.println("Berikut adalah menu Admin :");
         System.out.println("1. Tambah Stok Barang");
         System.out.println("2. Edit Stok Barang");
         System.out.println("3. Hapus Stok Barang");
-        System.out.println("4. Lihat Stok Barang");
+        System.out.println("4. Lihat Data Pelanggan Barang");
+        System.out.println("5. exit");
     }
 
-    public ArrayList<String> bukaMenu(ArrayList<String> stokData){
+    public ArrayList<String> bukaMenu(ArrayList<String> stokData, ArrayList<String> customerData){
         showStok(stokData);
         showMenu();
-        System.out.println("Masukan pilihan anda: ");
+        System.out.print("Masukan pilihan anda: ");
         int pilihan = input.nextInt();
         switch (pilihan){
             case 1:
@@ -32,7 +34,11 @@ public class AdminMenu {
             case 3:
                 stokData = hapusStok(stokData);
                 break;
+
             case 4:
+                showCustomerData(customerData);
+                break;
+            case 5:
                 return stokData;
             default:
                 System.out.println("Angka yang anda masukan tidak sesuai!");
@@ -42,11 +48,12 @@ public class AdminMenu {
 
     public ArrayList<String> tambahStok(ArrayList<String> stokData){
         showStok(stokData);
-        System.out.println("Masukan nama barang: ");
+        System.out.print("Masukan nama barang: ");
+        input.nextLine();
         String namaBarang = input.nextLine();
-        System.out.println("Masukan harga barang: ");
+        System.out.print("Masukan harga barang: ");
         int hargaBarang = input.nextInt();
-        System.out.println("Masukan stok barang: ");
+        System.out.print("Masukan stok barang: ");
         int stokBarang = input.nextInt();
         String formatedData = formatData(namaBarang, hargaBarang, stokBarang);
         stokData.add(formatedData);
@@ -55,13 +62,30 @@ public class AdminMenu {
 
     private void showStok(ArrayList<String> stokData){
         int counter = 1;
-        System.out.println("NO.\t NAMA\t HARGA\t STOK\t");
+        System.out.println(String.format("%2s %6s %10s %7s","NO.","NAMA" ,"HARGA" ,"STOK"));
         for(String data:stokData){
             String nama = getSpecificData(data, 0, 1);
             String harga = getSpecificData(data, 1, 2);
             String stok = getSpecificData(data, 2, 3);
+            System.out.println(String.format("%1s %10s %8s %5s",counter,nama,harga,stok));
+            counter++;
+        }
+    }
 
-            System.out.println(counter + ". " + nama + "\t" + harga + "\t" + stok);
+    private void showCustomerData(ArrayList<String> customerData){
+        int counter = 1;
+
+        System.out.println(String.format("%2s %8s %13s %5s %8s %15s %15s %15s","NO.","NAMA", "NAMA-BARANG","JUMLAH-SEWA","HARGA","SURAT-JAMINAN","TANGGAL-PENYEWAAN","TANGGAL-PENGEMBALIAN"));
+        for(String data : customerData){
+            String customerName = getSpecificData(data, 0, 1);
+            String objectName = getSpecificData(data, 1, 2);
+            String rentAmount = getSpecificData(data, 2, 3);
+            String price = getSpecificData(data, 3, 4);
+            String suratJaminan = getSpecificData(data, 4, 5);
+            String rentDate = getSpecificData(data, 5, 6);
+            String returnDate = getSpecificData(data, 6, 7);
+            System.out.println( String.format("%2s %10s %10s %6s %15s %10s %20s %18s",counter,customerName,objectName,rentAmount,price,suratJaminan,rentDate,returnDate));
+            counter++;
         }
     }
 
@@ -75,21 +99,21 @@ public class AdminMenu {
             counter++;
         }
         return counter;
-}
+    }
 
     private String getSpecificData(String data, int dataBeginingLocation, int dataEndLocation ) {
         return data.substring(getSpecificDataLocation(data,dataBeginingLocation),getSpecificDataLocation(data,dataEndLocation)-1);
-}
+    }
 
     public ArrayList<String> editStok(ArrayList<String> stokData){
         showStok(stokData);
-        System.out.println("Masukan no data yang ingin di edit: ");
+        System.out.print("Masukan no data yang ingin di edit: ");
         int indexData = input.nextInt();
-        System.out.println("Masukan nama data yang baru: ");
+        System.out.print("Masukan nama data yang baru: ");
         String dataBaru = input.nextLine();
-        System.out.println("Masukan harga barang yang baru: ");
+        System.out.print("Masukan harga barang yang baru: ");
         int hargaBaru = input.nextInt();
-        System.out.println("Masukan stok barang yang baru: ");
+        System.out.print("Masukan stok barang yang baru: ");
         int stokBaru = input.nextInt();
         String editedData = formatData(dataBaru, hargaBaru, stokBaru);
         stokData.set(indexData -1, editedData);
@@ -99,7 +123,7 @@ public class AdminMenu {
 
     public ArrayList<String> hapusStok(ArrayList<String> stokData){
         showStok(stokData);
-        System.out.println("Masukan nomor data yang ingin diedit: ");
+        System.out.print("Masukan nomor data yang ingin diedit: ");
         int indexData = input.nextInt();
         stokData.remove(indexData -1);
         return stokData;
--- aplikasipendataanpenyewaankamera/Main.java
+++ aplikasipendataanpenyewaankamera/Main.java
@@ -1,31 +1,37 @@
 package aplikasipendataanpenyewaankamera;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        fileStorageSystem manageFile = new fileStorageSystem();
-        AdminMenu admin = new AdminMenu();
-        ArrayList<String> stokData = manageFile.getRecords("stokdata.txt");
-        ArrayList<String> costumerData = manageFile.getRecords("costumerdata.txt");
-        while (true) {
-            System.out.println("Aplikasi pendataan Sewa Kamera");
-            System.out.println("Silahkan pilih login sebagai :");
-            System.out.println("1. Admin");
-            System.out.println("2. Pengguna");
-            Scanner input = new Scanner(System.in);
+    public static Scanner input = new Scanner(System.in);
 
+    public static void main(String[] args) throws IOException {
+
+        fileStorageSystem manageFile = new fileStorageSystem();
+
+        AdminMenu admin = new AdminMenu();
+
+        customerMenu customer = new customerMenu();
+
+        manageFile.createNecessaryDirectory();
+
+        ArrayList<String> stokData = manageFile.getRecords("stockDirectory/stockRecords.txt");
+
+        ArrayList<String> customerData = manageFile.getRecords("customerDirectory/customerRecords.txt");
+
+        while (true) {
+            showMenu();
             int pilihan = input.nextInt();
             switch (pilihan) {
                 case 1:
-                    stokData = admin.bukaMenu(stokData);
+                    stokData = admin.bukaMenu(stokData,customerData);
                     break;
 
                 case 2:
-                    //showemenu
+                    customerData = customer.placeOrder(customerData,stokData);
+                    stokData = customer.subtractStock(stokData);
                     break;
 
                 case 3:
@@ -35,8 +41,15 @@ public class Main {
                 default:
                     System.out.println("PIlihan yang anda masukan!");
             }
-            manageFile.storeData(costumerData, "costumerdata.txt", false);
-            manageFile.storeData(stokData, "stokdata.txt", false);
+            manageFile.storeCustomerStockData(customerData,stokData);
         }
+    }
+
+    private static void showMenu() {
+        System.out.println("Aplikasi pendataan Sewa Kamera");
+        System.out.println("Silahkan pilih login sebagai :");
+        System.out.println("1. Admin");
+        System.out.println("2. Pengguna");
+        System.out.print("Masukan Pilihan Anda : ");
     }
 }
